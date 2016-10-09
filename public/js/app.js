2c94c6b100a1bf082dd667b46361c952de3e0ef4
Mayank Punetha <Mayank Punetha>
2016-10-09 05:32:24 -0400

    Design changes for main page

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-var myApp = angular.module('myApp',[]);
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
@@ -12,10 +11,12 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
         }).error(function () {
 
         });
-    }
+    };
+
     $scope.newTask = function() {
 
-    }
+    };
+
     $scope.logout = function() {
         $http.post('/logout').then(function (res) {
             if (!res.data.success) {
@@ -26,6 +27,31 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
         }).error(function () {
 
         });
-    }
+    };
+
+    $scope.tasks = [];
+
+    // Add New Note
+    $scope.addNote = function(){
+        $scope.newNote = {};
+        $scope.newNote.createdOn = Date.now();
+        $scope.newNote.text = ' ';
+        $scope.newNote.edit = true;
+        $scope.notes.push($scope.newNote);
+        $scope.newNote = {};
+    };
+
+    // Delete Note
+    $scope.delete = function (i) {
+        var r = confirm("Are you sure you want to delete this note?");
+        if (r == true)
+            $scope.tasks.splice(i, 1);
+    };
+
+    // Update Note
+    $scope.update = function(i, note) {
+        $scope.tasks[i].text = note;
+        $scope.tasks[i].edit = false;
+    };
 
 }]);
